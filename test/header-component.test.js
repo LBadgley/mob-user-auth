18f3c81089427c7164ecc4f76c2377e5f0fd8e66
heather <unphurlng@gmail.com>
2019-03-11 13:46:25 -0700

    makeHeader() tests pass

--- test/header-component.test.js
+++ test/header-component.test.js
@@ -2,6 +2,20 @@ const test = QUnit.test;
 
 QUnit.module('TESTING OF HEADER CREATION');
 
+function makeHeader() {
+    const html = /*html*/ `
+        <div class="profile">
+            <h1>Books</h1>
+            <span id="user-name">Frick Goodnuff</span>
+            <button>Sign out</button>
+        </div>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+
 test('Creating Header', assert => {
     //arrange
     const expected = /*html*/ `
@@ -13,14 +27,8 @@ test('Creating Header', assert => {
     `;
 
     //act
-    const result = /*html*/ `
-        <div class="profile">
-            <h1>Books</h1>
-            <span id="user-name">Frick Goodnuff</span>
-            <button>Sign out</button>
-        </div>
-    `;
+    const result = makeHeader();
 
     //assert
-    assert.equal(result, expected);
+    assert.htmlEqual(result, expected);
 });
