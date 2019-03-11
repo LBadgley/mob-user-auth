1b7ed824c7e7aa1d0088f6659597342627425fd8
heather <unphurlng@gmail.com>
2019-03-11 14:14:13 -0700

    configure firebase and export auth

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: 'AIzaSyBW16xUDqVxKkVHXH0wd5uRfQhOCFr0ByA',
+    authDomain: 'user-auth-example-83692.firebaseapp.com'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
