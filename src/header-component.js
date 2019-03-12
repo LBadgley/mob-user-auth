c48381afe12428209ae2982bd25d8b7d573f63b2
heather <unphurlng@gmail.com>
2019-03-11 17:16:41 -0700

    Skip header profile auth on auth.js page

--- src/header-component.js
+++ src/header-component.js
@@ -30,11 +30,15 @@ export function makeProfile(user) {
 
 const headerContainer = document.getElementById('header-container');
 
-export default function loadHeader() {
+export default function loadHeader(options) {
     const dom = makeHeader();
     //we must reference elements before adding dom to the parent via appendChild
     const header = dom.querySelector('header');
     headerContainer.appendChild(dom);
+
+    if(options && options.skipAuth) {
+        return;
+    }
 
     auth.onAuthStateChanged(user => {
         if(user) {
